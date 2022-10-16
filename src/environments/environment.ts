3e3830e45f38b29cda2f99b5e8f2b5fd9ffbf1a3
Alfred Sopi <alfred.sopi@hotmail.com>
2022-10-16 11:16:55 +0200

    added NULL Address in environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   ETHEREUM_NETWORK: '<insert: mainnet | goerli>',
@@ -9,13 +5,5 @@ export const environment = {
   RENTAL_TOKEN: '<insert>',
   INFURA_ID: '<insert>',
   MORALIS_API_KEY: '<insert>',
+  NULL_ADDRESS: '0x0000000000000000000000000000000000000000',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
