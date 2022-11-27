9c1737bb64162b0e19283c0a856209d1ba732d92
Alfred Sopi <alfred.sopi@hotmail.com>
2022-11-27 17:17:18 +0100

    removed all unused spec files

--- src/app/pipes/format-address.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatAddressPipe } from './format-address.pipe';
-
-describe('FormatAddressPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatAddressPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
